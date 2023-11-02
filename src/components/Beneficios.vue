0cfd36ca2fad6b54873b2251df60bad4d4c3288a
jeffHQ <jeffry.hilario@utec.edu.pe>
2023-11-02 12:58:12 -0500

    jeff

--- src/components/Beneficios.vue
+++ src/components/Beneficios.vue
@@ -2,45 +2,45 @@
     <div class="beneficios">
         <h1 class="titulo">Beneficios</h1>
         <h3 class="subtitulo">Te brindamos un asesoramiento sobre un plan de Marketing que
-             te pueda a ayudar a poder analizar tus datos, hacerlo economico 
-             e idear un usuario ideal para tu negocio </h3>
-        
+            te pueda a ayudar a poder analizar tus datos, hacerlo economico
+            e idear un usuario ideal para tu negocio </h3>
+
         <div class="sub-beneficios">
             <div class="sub-sub-beneficios">
-                    <div class="icono">
-                        <img class="icono" :src="require('@/assets/check.png')">
-                    </div>
-                    <div class="texto">
-                        <h2>Analisis de Datos</h2>
-                        <h3>Análisis de datos La capacidad para analizar datos y métricas de 
-                            marketing te ayudará a tomar decisiones informadas y ajustar 
-                            tu estrategia según sea necesario</h3>
-                    </div>
+                <div class="icono">
+                    <img class="icono" :src="require('@/assets/check.png')">
                 </div>
-            <div class="sub-sub-beneficios">
-                    <div class="icono">
-                        <img class="icono" :src="require('@/assets/check.png')">
-                    </div>
-                    <div class="texto">
-                        <h2>Orientación al Cliente</h2>
-                        <h3>Pon al cliente en el centro de tus decisiones de marketing. 
-                            Escucha sus necesidades y preferencias y adapta tu enfoque en consecuencia</h3>
-                    </div>
+                <div class="texto">
+                    <h2>Analisis de Datos</h2>
+                    <h3>Análisis de datos La capacidad para analizar datos y métricas de
+                        marketing te ayudará a tomar decisiones informadas y ajustar
+                        tu estrategia según sea necesario</h3>
+                </div>
             </div>
             <div class="sub-sub-beneficios">
-                    <div class="icono">
-                        <img class="icono" :src="require('@/assets/check.png')">
-                    </div>
-                    <div class="texto">
-                        <h2>Estrategia Sólida</h2>
-                        <h3>Desarrollar una estrategia de marketing clara
-                             y bien definida te ayudará a establecer objetivos, 
-                             prioridades y un plan de acción coherente</h3>
-                    </div>
+                <div class="icono">
+                    <img class="icono" :src="require('@/assets/check.png')">
+                </div>
+                <div class="texto">
+                    <h2>Orientación al Cliente</h2>
+                    <h3>Pon al cliente en el centro de tus decisiones de marketing.
+                        Escucha sus necesidades y preferencias y adapta tu enfoque en consecuencia</h3>
+                </div>
+            </div>
+            <div class="sub-sub-beneficios">
+                <div class="icono">
+                    <img class="icono" :src="require('@/assets/check.png')">
+                </div>
+                <div class="texto">
+                    <h2>Estrategia Sólida</h2>
+                    <h3>Desarrollar una estrategia de marketing clara
+                        y bien definida te ayudará a establecer objetivos,
+                        prioridades y un plan de acción coherente</h3>
+                </div>
 
             </div>
         </div>
-            
+
 
     </div>
 </template>
@@ -49,20 +49,20 @@
 import { mapState } from 'vuex';
 
 export default {
-  name: 'beneficios',
-  components:{
-  },
-  computed: {
-    ...mapState(['name', 'picture']),
-  },
+    name: 'beneficios',
+    components: {
+    },
+    computed: {
+        ...mapState(['name', 'picture']),
+    },
 };
 </script>
 
 <style scoped>
-
 .texto {
     padding-left: 30px;
 }
+
 .titulo {
     padding-top: 50px;
 }
@@ -103,11 +103,13 @@ img {
     padding-left: 200px;
     padding-right: 200px;
 }
+
 .beneficios {
     background-color: white;
     display: flex;
     flex-direction: column;
     align-items: center;
     color: #b19a78;
+    padding-bottom: 50px;
 }
 </style>
--- src/components/PiePagina.vue
+++ src/components/PiePagina.vue
@@ -2,36 +2,47 @@
     <div class="piepagina">
       <div class="sub_div">
         <ul class="menu">
-          <li class="title_section">Sobre Nosotros</li>
-          <li class="part_section">Fundacion</li>
-          <li class="part_section">Objetivos</li>
-          <li class="part_section">Equipo</li>
+          <a class="title_section">Sobre Nosotros</a>
+          <a><router-link class="router-link" to="/">Fundacion</router-link></a>
+          <a><router-link class="router-link" to="/">Objetivos</router-link></a>
+          <a><router-link class="router-link" to="/">Equipo</router-link></a>
         </ul>
       </div>
       <div class="sub_div">
         <ul class="menu">
-          <li class="title_section">Documentación</li>
-          <li class="part_section">Guia de uso</li>
-          <li class="part_section">APIs</li>
+          <a class="title_section">Documentacion</a>
+          <a><router-link class="router-link" to="/">Guia de Uso</router-link></a>
+          <a><router-link class="router-link" to="/">API's</router-link></a>
         </ul>
       </div>
       <div class="sub_div">
         <ul class="menu">
-          <li class="title_section">Contacto</li>
-          <li class="part_section">Github</li>
-          <li class="part_section">Instagram</li>
-          <li class="part_section">Facebook</li>
+          <a class="title_section">Contacto</a>
+          <a><router-link class="router-link" to="/">GitHub</router-link></a>
+          <a><router-link class="router-link" to="/">Instagram</router-link></a>
+          <a><router-link class="router-link" to="/">Facebook</router-link></a>
         </ul>
       </div>
     </div>
   </template>
   
   <style scoped>
+
+  .router-link {
+    background-color: #d1c5ab;
+    padding: 0px;
+  }
+  a {
+    padding-top: 0px;
+    background-color: #d1c5ab;
+  }
   .piepagina {
     border: 3px solid;
     border-color: white;
     display: flex;
     background-color: #d1c5ab;
+    padding: 50px;
+    font-size: 1.2em;
   }
   
   .sub_div {
@@ -47,9 +58,16 @@
     align-self: flex-start; /* Alinea el título a la izquierda verticalmente */
   }
   
+  ul.menu {
+    display: flex;
+    flex-direction: column;
+    flex-wrap: nowrap;
+    align-items: flex-start;
+}
+
   .title_section {
     font-weight: bold;
-    color: #363634;
+    color: #524636;
   }
   
   .part_section {
