0977e3647f68e1b726402db139628b1c1ff62cbe
AlexandroChamochumbi <85258069+AlexandroChamochumbi@users.noreply.github.com>
2023-11-03 15:37:39 -0500

    Update ObjetivosView.vue

--- src/views/ObjetivosView.vue
+++ src/views/ObjetivosView.vue
@@ -2,23 +2,23 @@
   <div class="home">
     <div class="objetivos">
       OBJETIVOS
-      <h3>En esta sección, te presentamos nuestros principales objetivos:</h3>
+      <h3 class="titulo">En esta sección, te presentamos nuestros principales objetivos:</h3>
       <ul class="objetivos-grid">
         <li>
-          <p>Ayudar a las empresas a comprender mejor a sus clientes mediante el análisis de datos de imágenes.</p>
-          <img src="@/assets/objetivos1.png" alt="Imagen del Objetivo 1" style="width: 500px; height: auto;"/>
+          <p class="objetivo">Ayudar a las empresas a comprender mejor a sus clientes mediante el análisis de datos de imágenes</p>
+          <img src="@/assets/objetivos1.png" alt="Imagen del Objetivo 1" class="imagen-objetivo" style="width: 500px; height: 250px;"/>
         </li>
         <li>
-          <p>Facilitar el proceso de toma de decisiones de marketing a través de datos y métricas precisas.</p>
-          <img src="@/assets/objetivos2.jpg" alt="Imagen del Objetivo 2" style="width: 300px; height: auto;"/>
+          <p class="objetivo">Facilitar el proceso de toma de decisiones de marketing a través de datos y métricas precisas</p>
+          <img src="@/assets/objetivos2.jpg" alt="Imagen del Objetivo 2" class="imagen-objetivo" style="width: 500px; height: 250px;"/>
         </li>
         <li>
-          <p>Brindar asesoramiento para desarrollar estrategias de marketing efectivas y centradas en el cliente.</p>
-          <img src="@/assets/objetivos3.png" alt="Imagen del Objetivo 3" style="width: 400px; height: auto;"/>
+          <p class="objetivo">Brindar asesoramiento para desarrollar estrategias de marketing efectivas y centradas en el cliente</p>
+          <img src="@/assets/objetivos3.avif" alt="Imagen del Objetivo 3" class="imagen-objetivo" style="width: 500px; height: 250px;"/>
         </li>
         <li>
-          <p>Fomentar la creación de estrategias sólidas que ayuden a alcanzar metas y objetivos de marketing.</p>
-          <img src="@/assets/objetivos4.avif" alt="Imagen del Objetivo 4" style="width: 400px; height: auto;" />
+          <p class="objetivo">Fomentar la creación de estrategias sólidas que ayuden a alcanzar metas y objetivos de marketing</p>
+          <img src="@/assets/objetivos4.avif" alt="Imagen del Objetivo 4" class="imagen-objetivo" style="width: 500px; height: 250px;" />
         </li>
       </ul>
     </div>
@@ -46,26 +46,31 @@ export default {
   padding: 20px;
 }
 
-h3 {
-  font-size: 24px;
+.titulo {
+  font-size: 28px;
   margin: 20px 0;
+  font-weight: bold;
 }
 
 .objetivos-grid {
   display: grid;
-  grid-template-columns: repeat(2, 1fr); 
+  grid-template-columns: repeat(2, 1fr);
   grid-gap: 20px;
   list-style: none;
   padding: 0;
 }
 
-li {
-  font-size: 18px;
+.objetivo {
+  font-size: 16px;
   margin: 10px 0;
   text-align: center;
+  font-family: "Arial", sans-serif;
 }
 
-img {
+.imagen-objetivo {
   max-width: 100%;
+  border: 2px solid #d1c5ab;
+  border-radius: 10px;
 }
 </style>
+
