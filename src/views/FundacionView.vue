1dbe7b1e29aa0d29f590731461a359c37ee23f7b
AlexandroChamochumbi <85258069+AlexandroChamochumbi@users.noreply.github.com>
2023-11-03 16:58:56 -0500

    Update FundacionView.vue

--- src/views/FundacionView.vue
+++ src/views/FundacionView.vue
@@ -1,25 +1,54 @@
 <template>
-    <div class="home">
-        FUNDACION
+  <div class="home">
+    <div class="fundacion">
+      <h2>FUNDACIÓN</h2>
+      <div class="fundacion-content">
+        <div class= "color-bg">
+        <p>Nuestra fundación es una organización sin fines de lucro comprometida con la promoción y apoyo de iniciativas de marketing innovadoras. Trabajamos en estrecha colaboración con pequeñas y medianas empresas para ayudarles a alcanzar sus objetivos comerciales a través de estrategias de marketing efectivas.</p>
+        <p>En la Fundación, creemos en la importancia de la investigación y el análisis de datos para tomar decisiones informadas en marketing. Nuestro equipo de expertos se dedica a brindar asesoramiento y recursos que permiten a las empresas comprender mejor a sus clientes y mejorar sus estrategias de marketing.</p>
+        <p>Colaboramos con empresas de diversos sectores para fomentar la creación de estrategias sólidas que se adapten a sus necesidades y metas. Nuestro objetivo es contribuir al crecimiento y éxito de las empresas a través de la aplicación de prácticas de marketing efectivas y centradas en el cliente.</p>
+        </div>
+      </div>
     </div>
-    <PiePagina/>
-  </template>
-  
-  <script>
-  // @ is an alias to /src
-  import Presentacion from '@/components/Presentacion.vue'
-  import PiePagina from '@/components/PiePagina.vue'
-  import Beneficios from '@/components/Beneficios.vue'
-  export default {
-    name: 'fundacionview',
-    components: {
-      Presentacion,
-      PiePagina,
-      Beneficios
-    }
+  </div>
+  <PiePagina/>
+</template>
+
+
+<script>
+import Presentacion from '@/components/Presentacion.vue'
+import PiePagina from '@/components/PiePagina.vue'
+import Beneficios from '@/components/Beneficios.vue'
+export default {
+  name: 'fundacionview',
+  components: {
+    Presentacion,
+    PiePagina,
+    Beneficios
   }
-  </script>
-  <style>
-  
-  </style>
-  
+}
+</script>
+
+<style>
+.fundacion {
+  text-align: center;
+  padding: 20px;
+}
+
+.color-bg {
+  background-color: #b19a78; 
+  padding: 20px; 
+  border-radius: 10px; 
+  color: #fff; 
+}
+
+
+.fundacion h2 {
+  background-color: #d1c5ab;
+  padding: 10px;
+  border: 2px solid #524636;
+  border-radius: 10px;
+  font-size: 28px;
+  display: inline;
+}
+</style>
