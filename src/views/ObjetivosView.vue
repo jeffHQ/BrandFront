158163b721c53cf067b24b12f633335310f027b7
AlexandroChamochumbi <85258069+AlexandroChamochumbi@users.noreply.github.com>
2023-11-03 15:56:15 -0500

    Update ObjetivosView.vue

--- src/views/ObjetivosView.vue
+++ src/views/ObjetivosView.vue
@@ -4,19 +4,19 @@
       OBJETIVOS
       <h3 class="titulo">En esta sección, te presentamos nuestros principales objetivos:</h3>
       <ul class="objetivos-grid">
-        <li>
+        <li class="objetivo-li">
           <p class="objetivo">Ayudar a las empresas a comprender mejor a sus clientes mediante el análisis de datos de imágenes</p>
           <img src="@/assets/objetivos1.png" alt="Imagen del Objetivo 1" class="imagen-objetivo" style="width: 500px; height: 250px;"/>
         </li>
-        <li>
+        <li class="objetivo-li">
           <p class="objetivo">Facilitar el proceso de toma de decisiones de marketing a través de datos y métricas precisas</p>
-          <img src="@/assets/objetivos2.jpg" alt="Imagen del Objetivo 2" class="imagen-objetivo" style="width: 500px; height: 250px;"/>
+          <img src="@/assets/objetivos2.jpg" alt="Imagen del Objetivo 2" class="imagen-objetivo" style="width: 500px; height: 250px;" />
         </li>
-        <li>
+        <li class="objetivo-li">
           <p class="objetivo">Brindar asesoramiento para desarrollar estrategias de marketing efectivas y centradas en el cliente</p>
           <img src="@/assets/objetivos3.avif" alt="Imagen del Objetivo 3" class="imagen-objetivo" style="width: 500px; height: 250px;"/>
         </li>
-        <li>
+        <li class="objetivo-li">
           <p class="objetivo">Fomentar la creación de estrategias sólidas que ayuden a alcanzar metas y objetivos de marketing</p>
           <img src="@/assets/objetivos4.avif" alt="Imagen del Objetivo 4" class="imagen-objetivo" style="width: 500px; height: 250px;" />
         </li>
@@ -50,6 +50,7 @@ export default {
   font-size: 28px;
   margin: 20px 0;
   font-weight: bold;
+  color: #524636;
 }
 
 .objetivos-grid {
@@ -60,11 +61,17 @@ export default {
   padding: 0;
 }
 
+.objetivo-li {
+  background-color: #d1c5ab; 
+  border-radius: 10px;
+}
+
 .objetivo {
   font-size: 16px;
   margin: 10px 0;
   text-align: center;
   font-family: "Arial", sans-serif;
+  color: #363634;
 }
 
 .imagen-objetivo {
@@ -73,4 +80,3 @@ export default {
   border-radius: 10px;
 }
 </style>
-
