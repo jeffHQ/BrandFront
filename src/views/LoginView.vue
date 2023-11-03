879f5a8aedd2adb3f5f5cb0083d51d95ce5ea21a
jeffHQ <jeffry.hilario@utec.edu.pe>
2023-11-03 18:20:59 -0500

    ok

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,13 +1,19 @@
 <template>
   <div>
-    <button @click="login">Login Using Google</button>
+    <button @click="login" class="elegant-button">Login Using Google</button>
   </div>
+  <PiePagina/>
+  
 </template>
+
 
 <script setup>
 import { googleTokenLogin } from "vue3-google-login";
 import { useRouter } from "vue-router";
 import { useStore } from 'vuex';
+import PiePagina from '@/components/PiePagina.vue'
+
+
 
 const store = useStore();
 const router = useRouter();
@@ -36,5 +42,33 @@ const login = () => {
         console.error('Error al obtener datos del perfil del usuario:', error);
       });
   });
+}
+</script>
+
+<style scoped>
+.elegant-button {
+  background-color: #3498db; /* Color de fondo del botón */
+  color: #fff; /* Color del texto del botón */
+  padding: 10px 20px; /* Relleno interno del botón */
+  border: none; /* Borde del botón */
+  border-radius: 4px; /* Bordes redondeados */
+  cursor: pointer; /* Cambia el cursor al pasar sobre el botón */
+  transition: background-color 0.3s, transform 0.3s; /* Transición de color de fondo y transformación (escala) */
+}
+
+.elegant-button:hover {
+  background-color: #2188c3; /* Cambia el color de fondo cuando el cursor está sobre el botón */
+  transform: scale(1.1); /* Hace que el botón se agrande un poco al pasar el cursor */
+}
+</style>
+
+<script>
+import PiePagina from '@/components/PiePagina.vue'
+
+export default {
+  name: 'loginview', // Puedes darle un nombre al componente si lo deseas
+  components: {
+    PiePagina
+  }
 }
 </script>
--- src/views/PredecirView.vue
+++ src/views/PredecirView.vue
@@ -1,15 +1,18 @@
 <template>
-  <div>
+  <div class="predecir-view">
     <form @submit="enviarImagen">
-      <input type="file" ref="fileInput" accept="image/*" @change="cargarImagen" />
-      <button type="submit">Subir Imagen</button>
+      <label for="fileInput" class="file-label">
+        <input type="file" id="fileInput" ref="fileInput" accept="image/*" @change="cargarImagen" />
+        Subir Imagen
+      </label>
+      <img v-if="imagenSeleccionada" :src="imagenSeleccionadaUrl" class="image-preview" alt="Vista previa de la imagen" />
+      <button type="submit" class="upload-button">Enviar</button>
     </form>
   </div>
-  <PiePagina/>
+  <PiePagina />
 </template>
 
 <script>
-
 import PiePagina from '@/components/PiePagina.vue'
 
 export default {
@@ -17,6 +20,7 @@ export default {
   data() {
     return {
       imagenSeleccionada: null,
+      imagenSeleccionadaUrl: null,
     };
   },
   components: {
@@ -25,13 +29,24 @@ export default {
   methods: {
     cargarImagen(event) {
       this.imagenSeleccionada = event.target.files[0];
+      this.mostrarImagenSeleccionada();
+    },
+    mostrarImagenSeleccionada() {
+      if (this.imagenSeleccionada) {
+        const reader = new FileReader();
+        reader.readAsDataURL(this.imagenSeleccionada);
+        reader.onload = (e) => {
+          this.imagenSeleccionadaUrl = e.target.result;
+        };
+      } else {
+        this.imagenSeleccionadaUrl = null;
+      }
     },
     enviarImagen(event) {
       event.preventDefault();
       const formData = new FormData();
       formData.append('imagen', this.imagenSeleccionada);
 
-      // Reemplaza la URL con la dirección de tu API
       fetch('http://127.0.0.1:5000/predict', {
         method: 'POST',
         body: formData,
@@ -39,13 +54,56 @@ export default {
         .then(response => response.json())
         .then(data => {
           console.log('La imagen se ha subido con éxito:', data);
-          // Puedes realizar acciones adicionales aquí, como mostrar una confirmación al usuario.
         })
         .catch(error => {
           console.error('Error al subir la imagen:', error);
-          // Maneja los errores y proporciona retroalimentación al usuario si es necesario.
         });
     },
   },
 };
 </script>
+
+<style scoped>
+.predecir-view {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: #f7f7f7;
+  padding: 20px;
+}
+
+.file-label {
+  display: block;
+  margin-bottom: 20px;
+  text-align: center;
+  font-size: 16px;
+}
+
+#fileInput {
+  display: none;
+}
+
+.image-preview {
+  max-width: 1000px;
+  max-height: 300px;
+  margin-top: 10px;
+  width: 500px;
+}
+
+.upload-button {
+  background-color: #3498db;
+  color: #fff;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: background-color 0.3s, transform 0.3s;
+}
+
+.upload-button:hover {
+  background-color: #2188c3;
+  transform: scale(1.1);
+}
+</style>
